e07c1430923313837149f7926b7e8907a16da196
Mischa Dasberg <mischa@dasberg.nl>
2020-06-18 18:38:44 +0200

    ci: 🎡 run integration tests for webdriverio v5 and v6

--- itest/wdio-v6/test/wdio.ci.conf.js
+++ itest/wdio-v6/test/wdio.ci.conf.js
@@ -6,6 +6,7 @@ const defaultBrowserSauceOptions = {
     screenResolution: '1600x1200',
     seleniumVersion: '3.141.59',
 };
+config.maxInstances = 1;
 config.params = {
     environment: 'CI',
     default_directory: '/tmp'
